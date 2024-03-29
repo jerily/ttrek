cd12f3163fe426105f3bfbee5856dd10b38966d8
Neofytos Dimitriou <neo@jerily.cy>
2024-03-29 03:25:49 -0400

    added ksuid-tcl to install.sh

--- install.sh
+++ install.sh
@@ -182,8 +182,23 @@ if false; then
 fi
 
 # tsession
-if true; then
+if false; then
   git -C $BUILD_DIR clone https://github.com/jerily/tsession.git
   cd $BUILD_DIR/tsession
   make install PREFIX=$INSTALL_DIR > $BUILD_LOG_DIR/tsession-install.log 2>&1
+fi
+
+# ksuid-tcl
+if false; then
+  curl -L -o ksuid-tcl-1.0.3.tar.gz --output-dir $BUILD_DIR https://github.com/jerily/ksuid-tcl/archive/refs/tags/v1.0.3.tar.gz
+  tar -xzf $BUILD_DIR/ksuid-tcl-1.0.3.tar.gz -C $BUILD_DIR
+  cd $BUILD_DIR/ksuid-tcl-1.0.3
+  mkdir build
+  cd build
+  cmake .. \
+    -DTCL_LIBRARY_DIR=$INSTALL_DIR/lib \
+    -DTCL_INCLUDE_DIR=$INSTALL_DIR/include \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    -DCMAKE_PREFIX_PATH=$INSTALL_DIR/ > $BUILD_LOG_DIR/ksuid-tcl-configure.log 2>&1
+  make install > $BUILD_LOG_DIR/ksuid-tcl-install.log 2>&1
 fi
